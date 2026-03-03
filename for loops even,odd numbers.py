0a70621cf8c9780b70a9a188b30eb01a64c33c1a
priyamaurya85261-sudo <priyamaurya85261@gmail.com>
2026-03-03 22:48:53 +0530

    Add files via upload

---
+++ for loops even,odd numbers.py
@@ -0,0 +1,10 @@
+# this range prints even numbers from 1 to 20
+for i in range(1,20):
+    if i%2 == 0:
+        print(i)
+
+
+# this range prints odd numbers from 1 to 20
+for i in range(1,20):
+    if i%2 != 0:
+        print(i)
